7b077a8d6349352bc5f7c34d20e533fb8ed93dd7
Distribuidos <william.chavez@correo.icesi.edu.co>
2019-10-02 19:04:43 -0500

    Adding two more test cases
    	modificado:     test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -7,5 +7,7 @@ def test_add():
 # THEN I get the addition of two numbers
     add_result = demo.add(3,4)
     assert add_result == 7
+    assert demo.add(7,5) == 12
+    assert demo.add(14,26) == 40
 
 
