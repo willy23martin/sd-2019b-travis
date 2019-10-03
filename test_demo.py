f47981453ab2878b373f6d5a9f42a68ebceb5acf
Distribuidos <william.chavez@correo.icesi.edu.co>
2019-10-02 19:20:24 -0500

    Substraction between two numbers

--- test_demo.py
+++ test_demo.py
@@ -10,4 +10,14 @@ def test_add():
     assert demo.add(7,5) == 12
     assert demo.add(14,26) == 40
 
+def test_sub():
+    # GIVEN two numbers
+    # WHEN I invoke the method sub
+    # THEN I get the difference between the firstone and the other number
+
+    assert demo.sub(-14,25) == -39
+    assert demo.sub(24, 38) == -14
+    assert demo.sub(-45, -7) == -38
+    
+
 
